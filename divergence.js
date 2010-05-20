2730cd05c2c51db025ee28c816ab9df308657ced
Spencer Tipping <spencer@spencertipping.com>
2010-05-20 00:11:48 -0600

    Updated the JavaScript. Note that the operator-generated methods are currently a bit patchy.

--- divergence.js
+++ divergence.js
@@ -2,9 +2,9 @@
 // Licensed under the terms of the MIT source code license
 
 var d = (function () {
-  var d = function () {return d[d.default_action].apply (this, arguments)};
+  var c = {}, d = function () {return d[d.default_action].apply (this, arguments)};
   d.init = function (o) {for (var i = 1, l = arguments.length, $_; $_ = arguments[i], i < l; ++i) if ($_.call && $_.call.apply) $_.call (o);
-                                                                                                  else                          for (var k in $_) o[k] = $_[k]; return o};
+                                                                                                  else                          for (var k in $_) $_.hasOwnProperty (k) && (o[k] = $_[k]); return o};
   d.init (d, {inline_macros:  [],            id: function    (x) {return x},
                 functionals:  [],           arr: function    (o) {return Array.prototype.slice.call (o)},
                     aliases:  {},           map: function (o, f) {var x = {}; d.keys (o).each (function (k) {d.init (x, f (k, o[k]) || {})}); return x},
@@ -12,15 +12,15 @@ var d = (function () {
       functional_extensions:  {},     functions: function     () {var as = d.arr (arguments); return d.functionals.each (function (p) {d.init.apply (this, [p].concat (as))}), d},
                                    macro_expand: function    (s) {return d.inline_macros.fold (function (s, m) {return m(s)}, s)},
                                           alias: function (s, f) {d.aliases[s] = f.fn(); return d},
-                                          macro: function (r, f) {d.inline_macros.push (r.maps_to (f)); return d},
-                                          trace: function    (x) {d.tracer && d.tracer (x); return x}});
+                                          macro: function (r, f) {d.inline_macros.push (r.maps_to (f)); c = {}; return d},
+                                          trace: function    (x) {d.tracer && d.tracer ([x].concat (d.arr (arguments)).join (', ')); return x}});
 
-  d (String.prototype, (function (c) {return {maps_to: function (v) {var result = {}; result[this] = v; return result},
-                                               lookup: function  () {return '$0.split(".").fold("$0[$1]", $1)'.fn(this)},
-                                                alias: function (f) {return d.alias (this, f)},
-                                                 fail: function  () {throw new Error (this.toString())},
-                                                   fn: function  () {var s = d.macro_expand (this), f = d.aliases[s] || c[s] || (c[s] = eval ('(function(){return ' + s + '})'));
-                                                                     return f.fn.apply (f, arguments)}}}) ({}));
+  d (String.prototype, {maps_to: function (v) {var result = {}; result[this] = v; return result},
+                         lookup: function  () {return '$0.split(".").fold("$0[$1]", $1)'.fn(this)},
+                          alias: function (f) {return d.alias (this, f)},
+                           fail: function  () {throw new Error (this.toString())},
+                             fn: function  () {var s = this.toString(), f = d.aliases[s] || c[s] || (c[s] = eval ('(function(){return ' + d.macro_expand(s) + '})'));
+                                               return f.fn.apply (f, arguments)}});
 
   d (RegExp.prototype, {maps_to: function (f) {var s = this; return function (x) {return x.replace (s, f)}},
                           macro: function (f) {return d.macro (this, f)},
@@ -30,8 +30,8 @@ var d = (function () {
                         sort_by: function (f) {return this.sort ('$0($1) < $0($2)'.fn (f.fn()))},
                            each: function (f) {f = f.fn(); for (var i = 0, l = this.length; i < l; ++i) f (this[i]); return this},
                            grep: function (f) {var xs = [], f = f.fn(); for (var i = 0, l = this.length; i < l; ++i) f (this[i]) && xs.push (this[i]); return xs},
-                           fold: function (f) {var xs = d.arr (arguments), f = xs.shift().fn(), x = xs.length ? xs[0] : this[0];
-                                               for (var i = 1, l = xs.length + this.length; i < l; ++i) x = f (x, i < xs.length ? xs[i] : this[i - xs.length]); return x},
+                           fold: function (f) {var f = f.fn(), xl = arguments.length, x = xl > 1 ? arguments[1] : this[0];
+                                               for (var i = 2, l = xl + this.length; i < l; ++i) x = f (x, i < xl ? arguments[i] : this[i - xl]); return x},
                             map: function (f) {var xs = [], f = f.fn(); for (var i = 0, l = this.length; i < l; ++i) xs.push (f (this[i])); return xs},
                              fn: function  () {var xs = this, f = function () {return xs.map ('$1.fn().apply($_,$0)'.fn (arguments))}; return f.fn.apply (f, arguments)}});
 
@@ -45,7 +45,7 @@ var d = (function () {
            /\$(\d+)/g.macro ('"arguments[" + arguments[1] + "]"'.fn());
             /@(\w+)/g.macro ('"this." + $1'.fn());
 
-  /\{\|([\w,\s]+)\|/g.macro ('"(function(" + $1 + "){return "'.fn()); /\|\}/g.macro ('}).fn(arguments)');
+  /\{\|([\w,\s]+)\|/g.macro ('"(function(" + $1 + "){return "'.fn()); /\|\}/g.macro ('})');
               /\{\</g.macro ('(function(){return ');                  /\>\}/g.macro ('})');
 
   (d.functionals = [Array, Number, Boolean, Function, String, RegExp].map ('.prototype')).push (d.functional_extensions);
@@ -56,17 +56,15 @@ var d = (function () {
                                       '$0 + "_"': '"$_" + $0 + "$0"', '$0 + "_$"': '"{|xs, t| t.fn().apply($_,@_)" + $0 + "xs[0].fn().apply($_,@_)|}.fn($_)"'},
                           operators: {plus:'+', minus:'-', times:'*', over:'/', modulo:'%', lt:'<', gt:'>', le:'<=', ge:'>=', eq:'==', ne:'!=', req:'===', rne:'!==', and:'&&', or:'||', xor:'^',
                                       bitand:'&', bitor:'|', then:',', lshift: '<<', rshift: '>>', rushift: '>>>'}},
-                 unary: {transforms: {'$0':       '$0 + "$0"', '$0 + "$"': '"{|xs| " + $0 + "xs[0].fn().apply($_,@_)|}"',
-                                      '$0 + "_"': '$0 + "$_"', '$0 + "_$"': '"{|xs, t| " + $0 + "t.fn().apply($_,@_)|}"'},
+                 unary: {transforms: {'$0': '$0 + "($0 || $_)"', '$0 + "_fn"': '"{|f| " + $0 + "f.fn.apply($_,@_)|}.fn(($0 || $_).fn())"'},
                           operators: {not:'!', notnot:'!!', complement:'~', negative:'-', positive:'+'}},
-            assignment: {transforms: {'$0': '"$0" + $0 + "$1"'},
-                          operators: {plus_d:'+=', minus_d:'-=', times_d:'*=', over_d:'/=', bitand_d:'&=', bitor_d:'|=', bitxor_d:'^=', lshift_d:'<<=', rshift_d:'>>=', rushift_d:'>>>='}},
+            assignment: {transforms: {},
+                          operators: {plus_eq:'+=', minus_eq:'-=', times_eq:'*=', over_eq:'/=', bitand_eq:'&=', bitor_eq:'|=', bitxor_eq:'^=', lshift_eq:'<<=', rshift_eq:'>>=', rushift_eq:'>>>='}},
            applicative: {transforms: {'$0': '$0'},
                           operators: {'()': '$0($1)', '[]': '$0[$1]'}}}).create_aliases ();
   d.functions ({
       compose:  function (g) {var f = this.fn(); g = g.fn(); return function () {return f (g.apply (this, arguments))}},
  flat_compose:  function (g) {var f = this.fn(); g = g.fn(); return function () {return f.apply (this, g.apply (this, arguments))}},
-       plural:  function  () {return '$1.map ($0)'.fn(this)},
         curry:  function (n) {var f = this.fn(); return n > 1 ? function () {var as = d.arr(arguments); return function () {return f.curry (n - 1).apply (this, as.concat (d.arr (arguments)))}} : f},
         proxy:  function (g) {var f = this.fn(); return g ? function () {return f.apply.apply (f, g.fn() (this, arguments))} : function () {return f.apply (this, arguments)}},
          bind:  function (x) {var f = this.fn(); return d.init (function () {return f.apply (x, arguments)}, {binding: x, original: f})},
